b825f65367caaa2ee6b58a79b274cb499138e68f
SirKingBinx <bendawsdev@gmail.com>
2026-02-25 11:26:00 -0600

    Update cpuclocker.asm

--- cpuclocker.asm
+++ cpuclocker.asm
@@ -1,3 +1,7 @@
 ; quick script for grabbing the CPU speed. Press STOP after 1 sec
-inc ax
-jmp _start ; shortcut for line 1 if no start label is provided
+cpucycle:
+    inc ax
+    jmp cpucycle
+
+_start:
+    jmp cpucycle
