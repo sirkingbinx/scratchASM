68ce1cf2c68b2f6ae9c64af6cfde24fc266cc129
SirKingBinx <bendawsdev@gmail.com>
2026-02-24 22:50:51 -0600

    add files

---
+++ README.md
@@ -0,0 +1,9 @@
+# scab-scratch
+Scab is a mock Assembly language and virtual machine for Scratch. It is called scab for many reasons:
+- a name not taken by other big projects
+- is a *pain* to develop
+- an acronym for **Sc**ratch **A**ssem**b**ler
+
+This repository isn't actually the source code, it just holds the language tests.
+
+[Project link](https://scratch.mit.edu/projects/1283178575/)
---
+++ hello.asm
@@ -0,0 +1,14 @@
+; note: a start label is not required if your start function is at the beginning of your code, since
+;       code begins executing at mem address 1.
+;       if a start label is provided, the preprocessor will add a "JMP start" instruction into memory
+;       to make the code move to the starting point
+start:
+    ; syscall PRINT
+    MOV syc, 2
+    MOV syv, "hello world\n"
+    CALL
+
+    ; syscall EXIT
+    MOV syc, 1
+    MOV syv, 0
+    CALL
