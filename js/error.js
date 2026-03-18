9da763a475e6a167cba1b65ae07df9acaa6a13bf
SirKingBinx <bendawsdev@gmail.com>
2026-03-18 09:04:11 -0500

    JSDoc comments

--- js/error.js
+++ js/error.js
@@ -1,14 +1,31 @@
 // scratch-vm-js/error.js - Error class, for error information stuff
 // (C) Copyright 2026 SirKingBinx / MIT License
 
-const errorData = require("./errors.json");
+import errors from "./errors.json" with { type: 'json' }
 
 export class VMError {
+    /**
+     * @private
+     */
     constructor(errorCode) {
-        const data = errorData[errorCode.toString()];
+        const data = errors[errorCode.toString()];
 
-        this.id = errorCode;
+        /**
+         * @type { number }
+         * @description The specific error ID thrown. This will be unique to the source of the error.
+         */
+        this.errorId = errorCode;
+
+        /**
+         * @type { string }
+         * @description The error message attached to this error ID. This is a more generic error message which may help you debug your code.
+         */
         this.message = data.message;
+        
+        /**
+         * @type { string }
+         * @description The internal function call that generated this error. This will be unique for each error code.
+         */
         this.source = data.source;
     }
 }
--- js/scratchVM.js
+++ js/scratchVM.js
@@ -3,10 +3,25 @@
 
 import { ScratchVMState } from "./vmstate";
 
+/**
+ * The current version of scratch-vm-js that is installed.
+ */
 export const version = "1.0.0";
+
+/**
+ * The target scratch-VM assembly version of the compiler.
+ */
+export const target_version = "1.1";
+
+/**
+ * The repository attached to this version of scratch-vm-js.
+ */
 export const repository = "https://github.com/sirkingbinx/scratch-vm";
 
-/** @type {number} memSize The amount of memory (in bytes) to allocate. */
+/**
+ * @param {number} memSize The amount of memory (in bytes) to allocate.
+ * @description Creates a new VM state for loading, executing, and debugging code.
+ */
 export function createState(memSize = 16000) {
     return new ScratchVMState(memSize);
 }
